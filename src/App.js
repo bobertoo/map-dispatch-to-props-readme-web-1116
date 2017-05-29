79ad77ac90a564a5f6c2c206982a48d22d2ebc32
Vinny Martinez <vinny@vinnymartinez.com>
2017-05-29 15:48:17 -0400

    mapped dispatch to props and binded action creators

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import { connect } from 'react-redux';
-import { addItem } from  './actions/items'
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {addItem} from './actions/items'
 
 class App extends Component {
-  handleOnClick(){
-    this.props.store.dispatch(addItem())
+  handleOnClick() {
+    this.props.addItem()
   }
   render() {
     return (
       <div className="App">
-          <button onClick={this.handleOnClick.bind(this)}>Click</button>
-          <p> {this.props.items.length}</p>
+        <button onClick={this.handleOnClick.bind(this)}>Click</button>
+        <p>
+          {this.props.items.length}</p>
       </div>
     );
   }
 }
 
-const connectedComponent = connect(mapStateToProps)(App)
+const connectedComponent = connect(mapStateToProps, mapDispatchToProps)(App)
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {items: state.items}
 }
 
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    addItem: addItem
+  }, dispatch)
+}
 
 export default connectedComponent;
